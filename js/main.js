91c8ca7f44572ce71c1f76b7d153bd1c3960bfe9
Noelle Matthews <nematthews@willamette.edu>
2025-07-08 21:42:18 -0700

    updated contact me form v3

--- js/main.js
+++ js/main.js
@@ -330,63 +330,62 @@
     }
 
     function sendMail() {
-        $('.contact-form [type="submit"]').on('click', function () {
+    $('.contact-form [type="submit"]').on('click', function () {
 
-            var emailVal = $('#contact-email').val();
+        var emailVal = $('#contact-email').val();
 
-            if (isValidEmailAddress(emailVal)) {
-                var params = {
-                    'action': 'SendMessage',
-                    'name': $('#name').val(),
-                    'email': $('#contact-email').val(),
-                    'subject': $('#subject').val(),
-                    'message': $('#message').val()
-                };
-                $.ajax({
-                    type: "POST",
-                    url: "php/sendMail.php",
-                    data: params,
-                    success: function (response) {
-                        if (response) {
-                            var responseObj = $.parseJSON(response);
-                            if (responseObj.ResponseData)
-                            {
-                                alert(responseObj.ResponseData);
-                            }
-                        }
-                    },
-                    error: function (xhr, ajaxOptions, thrownError) {
-                        //xhr.status : 404, 303, 501...
-                        var error = null;
-                        switch (xhr.status)
+        if (isValidEmailAddress(emailVal)) {
+            var params = {
+                'action': 'SendMessage',
+                'name': $('#name').val(),
+                'email': $('#contact-email').val(),
+                'subject': $('#subject').val(),
+                'message': $('#message').val()
+            };
+            $.ajax({
+                type: "POST",
+                url: "php/sendMail.php",
+                data: params,
+                success: function (response) {
+                    if (response) {
+                        var responseObj = $.parseJSON(response);
+                        if (responseObj.ResponseData)
                         {
-                            case "301":
-                                error = "Redirection Error!";
-                                break;
-                            case "307":
-                                error = "Error, temporary server redirection!";
-                                break;
-                            case "400":
-                                error = "Bad request!";
-                                break;
-                            case "404":
-                                error = "Page not found!";
-                                break;
-                            case "500":
-                                error = "Server is currently unavailable!";
-                                break;
-                            default:
-                                error = "Unespected error, please try again later.";
-                        }
-                        if (error) {
-                            alert(error);
+                            alert(responseObj.ResponseData);
                         }
                     }
-                });
-            } else {
-                alert('Your email is not in valid format');
-            }
-        });
-    }
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    var error = null;
+                    switch (xhr.status)
+                    {
+                        case "301":
+                            error = "Redirection Error!";
+                            break;
+                        case "307":
+                            error = "Error, temporary server redirection!";
+                            break;
+                        case "400":
+                            error = "Bad request!";
+                            break;
+                        case "404":
+                            error = "Page not found!";
+                            break;
+                        case "500":
+                            error = "Server is currently unavailable!";
+                            break;
+                        default:
+                            error = "Unespected error, please try again later.";
+                    }
+                    if (error) {
+                        alert(error);
+                    }
+                }
+            });
+        } else {
+            alert('Your email is not in valid format');
+        }
+    });
+}
 
 })(jQuery);
